2dafc6c2a01f59bda460769aaa1c766fd6f85c8d
刘祥超 <iwind.liu@gmail.com>
2018-10-18 19:33:44 +0800

    增加TeaWeb日志功能

--- src/main/init.sh
+++ src/main/init.sh
@@ -6,7 +6,7 @@ export GOPATH=`pwd`/../../
 
 # go_get function
 function go_get() {
-    echo "go get ${1}"
+    echo "go get ${1} ..."
     if [ ! -d ${GOPATH}/${1} ]
     then
         go get ${1}
@@ -29,6 +29,7 @@ go_get "github.com/pbnjay/memory"
 
 # mongodb
 go_get "github.com/mongodb/mongo-go-driver"
+echo "   Don't worry, you can ignore 'no Go files' warning in mongodb"
 
 # usa parser
 go_get "github.com/ua-parser/uap-go"
@@ -36,7 +37,11 @@ go_get "github.com/ua-parser/uap-go"
 # system cpu, memory, disk ...
 go_get "github.com/shirou/gopsutil"
 
+# javascript
+go_get "github.com/robertkrimen/otto"
+
 # TeaWeb
 go_get "github.com/TeaWeb/code"
+echo "   Don't worry, you can ignore 'no Go files' warning in TeaWeb code"
 
 echo "[done]"
---
+++ src/main/views/@default/log/runtime.js
@@ -0,0 +1,36 @@
+Tea.context(function () {
+    var that = this;
+
+    this.logText = "";
+    this.stopScrolling = false;
+
+    this.$delay(function () {
+        this.$find(".log-box").bind("wheel", function () {
+            if (this.scrollHeight < this.scrollTop + this.offsetHeight + 10) {
+                that.stopScrolling = false;
+            } else {
+                that.stopScrolling = true;
+            }
+        });
+        this.$find(".log-box").bind("scroll", function () {
+
+        });
+        this.load();
+    });
+
+    this.load = function () {
+        if (!this.stopScrolling) {
+            this.$find(".log-box")[0].scrollTop = 10000;
+        }
+
+        this.$post(".runtime")
+            .success(function (resp) {
+                this.logText = this.logText + resp.data.data.replace(/\n/g, "<br/>");
+            })
+            .done(function () {
+                this.$delay(function () {
+                    this.load();
+                }, 1000);
+            });
+    };
+});
