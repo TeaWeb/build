44bee715e9bc722753b9fde91e3f6c3ec05ddbac
刘祥超 <iwind.liu@gmail.com>
2018-12-28 23:49:17 +0800

    实现根据错误数自动下线后端服务器

--- src/main/views/@default/proxy/backend/index.js
+++ src/main/views/@default/proxy/backend/index.js
@@ -16,4 +16,34 @@ Tea.context(function () {
                 "backendId": backendId
             });
     };
+
+    this.putOnline = function (backend) {
+        if (!window.confirm("确定要上线此服务器吗？")) {
+            return;
+        }
+        this.$post("/proxy/backend/online")
+            .params({
+                "server": this.proxy.filename,
+                "backendId": backend.id
+            })
+            .success(function () {
+                backend.isDown = false;
+                backend.currentFails = 0;
+            });
+    };
+
+    this.clearFails = function (backend) {
+        if (!window.confirm("确定要清除此服务器的失败次数吗？")) {
+            return;
+        }
+        this.$post("/proxy/backend/clearFails")
+            .params({
+                "server": this.proxy.filename,
+                "backendId": backend.id
+            })
+            .success(function () {
+                backend.isDown = false;
+                backend.currentFails = 0;
+            });
+    };
 });
