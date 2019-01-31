aaf1261dcc104cfa7aee9c47c92bb4128a119c08
刘祥超 <iwind.liu@gmail.com>
2019-01-31 10:59:47 +0800

    改进界面

--- src/main/views/@default/layout.js
+++ src/main/views/@default/layout.js
@@ -20,10 +20,16 @@ Tea.context(function () {
 		this.renewNoticeBadge();
 	});
 
+	var documentTitle = document.title;
 	this.renewNoticeBadge = function () {
 		this.$get("/notices/badge")
 			.success(function (resp) {
 				this.countNoticesBadge = resp.data.count;
+				if (this.countNoticesBadge > 0) {
+					document.title = documentTitle + "(" + this.countNoticesBadge + "通知)";
+				} else {
+					document.title = documentTitle;
+				}
 			})
 			.done(function () {
 				this.$delay(function () {
