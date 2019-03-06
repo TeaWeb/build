74e09da483c2a6f554ea71c4f00744bf8e6a1ddd
刘祥超 <iwind.liu@gmail.com>
2019-03-06 19:46:55 +0800

    [proxy]优化路径规则

--- src/main/views/@default/proxy/locations/index.js
+++ src/main/views/@default/proxy/locations/index.js
@@ -1,31 +1,63 @@
 Tea.context(function () {
-    this.location = null;
+	this.$delay(function () {
+		this.sortable();
+	});
 
-    this.deleteLocation = function (locationId) {
-        if (!window.confirm("确定要删除此路径配置吗？")) {
-            return;
-        }
-        this.$post("/proxy/locations/delete")
-            .params({
-                "serverId": this.server.id,
-                "locationId": locationId
-            })
-            .refresh();
-    };
+	this.location = null;
 
-    this.moveUp = function (index) {
-        this.$post("/proxy/locations/moveUp")
-            .params({
-                "serverId": this.server.id,
-                "index": index
-            });
-    };
+	this.deleteLocation = function (locationId) {
+		if (!window.confirm("确定要删除此路径配置吗？")) {
+			return;
+		}
+		this.$post("/proxy/locations/delete")
+			.params({
+				"serverId": this.server.id,
+				"locationId": locationId
+			})
+			.refresh();
+	};
 
-    this.moveDown = function (index) {
-        this.$post("/proxy/locations/moveDown")
-            .params({
-                "serverId": this.server.id,
-                "index": index
-            });
-    };
+	this.moveUp = function (index) {
+		this.$post("/proxy/locations/moveUp")
+			.params({
+				"serverId": this.server.id,
+				"index": index
+			});
+	};
+
+	this.moveDown = function (index) {
+		this.$post("/proxy/locations/moveDown")
+			.params({
+				"serverId": this.server.id,
+				"index": index
+			});
+	};
+
+	/**
+	 * 拖动排序
+	 */
+	this.sortable = function () {
+		if (this.locations.length == 0) {
+			return;
+		}
+		var box = this.$find("#locations-table")[0];
+		var that = this;
+		Sortable.create(box, {
+			draggable: "tbody",
+			handle: ".icon.handle",
+			onStart: function () {
+
+			},
+			onUpdate: function (event) {
+				var newIndex = event.newIndex;
+				var oldIndex = event.oldIndex;
+				that.$post("/proxy/locations/move")
+					.params({
+						"serverId": that.server.id,
+						"fromIndex": oldIndex,
+						"toIndex": newIndex
+					});
+			}
+		});
+	};
 });
