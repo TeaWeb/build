dee60e3904655966977e804462c7e7b1a5e9080d
刘祥超 <iwind.liu@gmail.com>
2019-04-10 20:59:03 +0800

    [mongo]MongoDB设置支持认证机制选择

--- src/main/views/@default/settings/mongo/update.js
+++ src/main/views/@default/settings/mongo/update.js
@@ -1,40 +1,51 @@
 Tea.context(function () {
-   this.authUpdating = this.config.username != null && this.config.username.length > 0;
-   this.isTesting = false;
-   this.testingError = "";
-   this.testingSuccess = "";
+	this.authUpdating = this.config.username != null && this.config.username.length > 0;
+	this.isTesting = false;
+	this.testingError = "";
+	this.testingSuccess = "";
 
-   this.updateAuth = function () {
-        this.authUpdating = !this.authUpdating;
-   };
+	if (this.config.authMechanism.length == 0) {
+		this.config.authMechanism = "SCRAM-SHA-1";
+	}
 
-   this.testConnection = function () {
-        var params = {
-            host: this.config.host,
-            port: this.config.port
-        };
-        if (this.authUpdating) {
-            params["username"] = this.config.username;
-            params["password"] = this.config.password;
-        }
+	this.updateAuth = function () {
+		this.authUpdating = !this.authUpdating;
+	};
 
-        this.isTesting = true;
-        this.testingError = "";
-        this.testingSuccess = "";
+	this.testConnection = function () {
+		var params = {
+			host: this.config.host,
+			port: this.config.port
+		};
+		if (this.authUpdating) {
+			if (this.config.username.length == 0) {
+				alert("请输入用户名");
+				this.$find("form input[name='username']").focus();
+				return;
+			}
+			params["username"] = this.config.username;
+			params["password"] = this.config.password;
+			params["authMechanism"] = this.config.authMechanism;
+			params["authMechanismProperties"] = this.config.authMechanismProperties;
+		}
 
-        this.$get(".test")
-            .params(params)
-            .success(function () {
-                this.testingError = "";
-                this.testingSuccess = "连接成功！";
-            })
-            .fail(function (resp) {
-                if (resp) {
-                    this.testingError = resp.message;
-                }
-            })
-            .done(function () {
-                this.isTesting = false;
-            });
-   };
+		this.isTesting = true;
+		this.testingError = "";
+		this.testingSuccess = "";
+
+		this.$get(".test")
+			.params(params)
+			.success(function () {
+				this.testingError = "";
+				this.testingSuccess = "连接成功！";
+			})
+			.fail(function (resp) {
+				if (resp) {
+					this.testingError = resp.message;
+				}
+			})
+			.done(function () {
+				this.isTesting = false;
+			});
+	};
 });
