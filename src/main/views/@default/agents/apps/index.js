71e1dbf2c682f65ad115bc4394b501c5b6de9e58
刘祥超 <iwind.liu@gmail.com>
2019-04-24 16:04:58 +0800

    [agent]在App列表页可以添加系统App（如果没有系统App的情况下）

--- src/main/views/@default/agents/apps/index.js
+++ src/main/views/@default/agents/apps/index.js
@@ -1,13 +1,32 @@
 Tea.context(function () {
-    this.deleteApp = function (appId) {
-        if (!window.confirm("确定要删除此App吗？")) {
-            return;
-        }
-        this.$post("/agents/apps/delete")
-            .params({
-                "agentId": this.agentId,
-                "appId": appId
-            })
-            .refresh();
-    };
+	this.hasSystemApp = false;
+
+	if (this.apps != null) {
+		this.hasSystemApp = this.apps.$exist(function (k, v) {
+			return v.id == "system";
+		});
+	}
+
+	this.deleteApp = function (appId) {
+		if (!window.confirm("确定要删除此App吗？")) {
+			return;
+		}
+		this.$post("/agents/apps/delete")
+			.params({
+				"agentId": this.agentId,
+				"appId": appId
+			})
+			.refresh();
+	};
+
+	this.addSystemApp = function () {
+		if (!window.confirm("确定要添加内置的系统App吗？")) {
+			return;
+		}
+		this.$post("/agents/board/initDefaultApp")
+			.params({
+				"agentId": this.agentId
+			})
+			.refresh();
+	};
 });
