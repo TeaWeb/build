412118e86a1cca80e7e256d851817cff419d1cf5
刘祥超 <iwind.liu@gmail.com>
2019-05-04 22:46:22 +0800

    [proxy]支持自定义TLS加密算法套件

--- src/main/web/views/@default/proxy/ssl/index.js
+++ src/main/web/views/@default/proxy/ssl/index.js
@@ -1,77 +1,77 @@
 Tea.context(function () {
-    /**
-     * SSL管理
-     */
-    this.showSSLOptions = (this.server.ssl != null) ? this.server.ssl.on : false;
-    this.sslCertFile = null;
-    this.sslCertEditing = false;
+	/**
+	 * SSL管理
+	 */
+	this.showSSLOptions = (this.server.ssl != null) ? this.server.ssl.on : false;
+	this.sslCertFile = null;
+	this.sslCertEditing = false;
 
-    this.sslKeyFile = null;
-    this.sslKeyEditing = false;
+	this.sslKeyFile = null;
+	this.sslKeyEditing = false;
 
-    this.switchSSLOn = function () {
-        var message = (this.server.ssl != null && this.server.ssl.on) ? "确定要关闭HTTPS吗？" : "确定要开启HTTPS吗？";
-        if (!window.confirm(message)) {
-            return;
-        }
+	this.switchSSLOn = function () {
+		var message = (this.server.ssl != null && this.server.ssl.on) ? "确定要关闭HTTPS吗？" : "确定要开启HTTPS吗？";
+		if (!window.confirm(message)) {
+			return;
+		}
 
-        this.showSSLOptions = !this.showSSLOptions;
-        if (this.server.ssl == null) {
-            this.server.ssl = { "on": this.showSSLOptions };
-        } else {
-            this.server.ssl.on = !this.server.ssl.on;
-        }
+		this.showSSLOptions = !this.showSSLOptions;
+		if (this.server.ssl == null) {
+			this.server.ssl = {"on": this.showSSLOptions};
+		} else {
+			this.server.ssl.on = !this.server.ssl.on;
+		}
 
-        if (this.server.ssl.on) {
-            this.$post("/proxy/ssl/on")
-                .params({
-                    "serverId": this.server.id
-                });
-        } else {
-            this.$post("/proxy/ssl/off")
-                .params({
-                    "serverId": this.server.id
-                });
-        }
-    };
+		if (this.server.ssl.on) {
+			this.$post("/proxy/ssl/on")
+				.params({
+					"serverId": this.server.id
+				});
+		} else {
+			this.$post("/proxy/ssl/off")
+				.params({
+					"serverId": this.server.id
+				});
+		}
+	};
 
-    this.changeSSLCertFile = function (event) {
-        if (event.target.files.length > 0) {
-            this.sslCertFile = event.target.files[0];
-        }
-    };
+	this.changeSSLCertFile = function (event) {
+		if (event.target.files.length > 0) {
+			this.sslCertFile = event.target.files[0];
+		}
+	};
 
-    this.uploadSSLCertFile = function () {
-        if (this.sslCertFile == null) {
-            alert("请先选择证书文件");
-            return;
-        }
+	this.uploadSSLCertFile = function () {
+		if (this.sslCertFile == null) {
+			alert("请先选择证书文件");
+			return;
+		}
 
-        this.$post("/proxy/ssl/uploadCert")
-            .params({
-                "serverId": this.server.id,
-                "certFile": this.sslCertFile
-            });
-    };
+		this.$post("/proxy/ssl/uploadCert")
+			.params({
+				"serverId": this.server.id,
+				"certFile": this.sslCertFile
+			});
+	};
 
-    this.changeSSLKeyFile = function (event) {
-        if (event.target.files.length > 0) {
-            this.sslKeyFile = event.target.files[0];
-        }
-    };
+	this.changeSSLKeyFile = function (event) {
+		if (event.target.files.length > 0) {
+			this.sslKeyFile = event.target.files[0];
+		}
+	};
 
-    this.uploadSSLKeyFile = function () {
-        if (this.sslKeyFile == null) {
-            alert("请先选择密钥文件");
-            return;
-        }
+	this.uploadSSLKeyFile = function () {
+		if (this.sslKeyFile == null) {
+			alert("请先选择密钥文件");
+			return;
+		}
 
-        this.$post("/proxy/ssl/uploadKey")
-            .params({
-                "serverId": this.server.id,
-                "keyFile": this.sslKeyFile
-            });
-    };
+		this.$post("/proxy/ssl/uploadKey")
+			.params({
+				"serverId": this.server.id,
+				"keyFile": this.sslKeyFile
+			});
+	};
 
 	/**
 	 * 启动
@@ -100,5 +100,12 @@ Tea.context(function () {
 			.success(function () {
 				window.location.reload();
 			});
+	};
+
+	/**
+	 * 加密算法套件
+	 */
+	this.formatCipherSuite = function (cipherSuite) {
+		return cipherSuite.replace(/(AES|3DES)/, "<var>$1</var>");
 	};
 });
--- src/main/web/views/@default/proxy/ssl/update.js
+++ src/main/web/views/@default/proxy/ssl/update.js
@@ -1,40 +1,44 @@
 Tea.context(function () {
-    this.httpsOn = (this.server.ssl != null && this.server.ssl.on);
+	this.$delay(function () {
+		this.sortable();
+	});
 
-    if (this.server.ssl == null) {
-       this.server.ssl = {
-           "certificate": "",
-           "certificateKey": "",
-           "listen": []
-       };
-    }
-    if (this.server.ssl.listen == null) {
-        this.server.ssl.listen = [];
-    }
-    if (this.server.ssl.listen.length == 0) {
-        this.server.ssl.listen = [ "0.0.0.0:443" ];
-    }
+	this.httpsOn = (this.server.ssl != null && this.server.ssl.on);
 
-    this.submitSuccess = function () {
-        alert("修改成功");
+	if (this.server.ssl == null) {
+		this.server.ssl = {
+			"certificate": "",
+			"certificateKey": "",
+			"listen": []
+		};
+	}
+	if (this.server.ssl.listen == null) {
+		this.server.ssl.listen = [];
+	}
+	if (this.server.ssl.listen.length == 0) {
+		this.server.ssl.listen = ["0.0.0.0:443"];
+	}
 
-        window.location = "/proxy/ssl?serverId=" + this.server.id;
-    };
+	this.submitSuccess = function () {
+		alert("修改成功");
 
-    /**
-     * 绑定的网络地址
-     */
-    this.listenAdding = false;
-    this.addingListenName = "";
+		window.location = "/proxy/ssl?serverId=" + this.server.id;
+	};
+
+	/**
+	 * 绑定的网络地址
+	 */
+	this.listenAdding = false;
+	this.addingListenName = "";
 	this.editingListenIndex = -1;
 
-    this.addListen = function () {
-        this.listenAdding = true;
+	this.addListen = function () {
+		this.listenAdding = true;
 		this.editingListenIndex = -1;
-        this.$delay(function () {
-            this.$find("form input[name='addingListenName']").focus();
-        });
-    };
+		this.$delay(function () {
+			this.$find("form input[name='addingListenName']").focus();
+		});
+	};
 
 	this.editListen = function (index) {
 		this.listenAdding = true;
@@ -45,29 +49,91 @@ Tea.context(function () {
 		this.addingListenName = this.server.ssl.listen[index];
 	};
 
-    this.confirmAddListen = function () {
-        this.addingListenName = this.addingListenName.trim();
-        if (this.addingListenName.length == 0) {
-            alert("网络地址不能为空");
-            this.$find("form input[name='addingListenName']").focus();
-            return;
-        }
+	this.confirmAddListen = function () {
+		this.addingListenName = this.addingListenName.trim();
+		if (this.addingListenName.length == 0) {
+			alert("网络地址不能为空");
+			this.$find("form input[name='addingListenName']").focus();
+			return;
+		}
 		if (this.editingListenIndex > -1) {
 			this.server.ssl.listen[this.editingListenIndex] = this.addingListenName;
 		} else {
 			this.server.ssl.listen.push(this.addingListenName);
 		}
-        this.cancelListenAdding();
-    };
-
-    this.cancelListenAdding = function () {
-		this.listenAdding = false;
-        this.addingListenName = "";
-        this.editingListenIndex = -1;
-    };
-
-    this.removeListen = function (index) {
-        this.server.ssl.listen.$remove(index);
 		this.cancelListenAdding();
-    };
+	};
+
+	this.cancelListenAdding = function () {
+		this.listenAdding = false;
+		this.addingListenName = "";
+		this.editingListenIndex = -1;
+	};
+
+	this.removeListen = function (index) {
+		this.server.ssl.listen.$remove(index);
+		this.cancelListenAdding();
+	};
+
+	/**
+	 * 高级设置
+	 */
+	this.advancedOptionsVisible = false;
+	this.cipherSuitesOn = (this.server.ssl.cipherSuites != null && this.server.ssl.cipherSuites.length > 0);
+	this.selectedCipherSuites = [];
+	if (this.server.ssl.cipherSuites != null && this.server.ssl.cipherSuites.length > 0) {
+		this.selectedCipherSuites = this.server.ssl.cipherSuites;
+	}
+	var allCipherSuites = this.cipherSuites.$copy();
+	if (this.selectedCipherSuites.length > 0) {
+		var that = this;
+		this.cipherSuites = allCipherSuites.$findAll(function (k, v) {
+			return !that.selectedCipherSuites.$contains(v);
+		});
+	}
+
+	this.showAdvancedOptions = function () {
+		this.advancedOptionsVisible = !this.advancedOptionsVisible;
+	};
+
+	this.addCipherSuite = function (cipherSuite) {
+		if (!this.selectedCipherSuites.$contains(cipherSuite)) {
+			this.selectedCipherSuites.push(cipherSuite);
+		}
+		this.cipherSuites.$removeValue(cipherSuite);
+	};
+
+	this.removeCipherSuite = function (cipherSuite) {
+		if (!window.confirm("确定要删除此套件吗？")) {
+			return;
+		}
+		this.selectedCipherSuites.$removeValue(cipherSuite);
+
+		var that = this;
+		this.cipherSuites = allCipherSuites.$findAll(function (k, v) {
+			return !that.selectedCipherSuites.$contains(v);
+		});
+	};
+
+	this.formatCipherSuite = function (cipherSuite) {
+		return cipherSuite.replace(/(AES|3DES)/, "<var>$1</var>");
+	};
+
+	/**
+	 * 拖动排序
+	 */
+	this.sortable = function () {
+		var box = this.$find(".cipher-suites-box")[0];
+		var that = this;
+		Sortable.create(box, {
+			draggable: ".label",
+			handle: ".icon.handle",
+			onStart: function () {
+
+			},
+			onUpdate: function (event) {
+
+			}
+		});
+	};
 });
