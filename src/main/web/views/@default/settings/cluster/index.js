d5f0c474bca7602b7ed8c1d902630db90c071b4f
刘祥超 <iwind.liu@gmail.com>
2019-05-15 11:49:48 +0800

    [cluster]可以手动同步数据到cluster

--- src/main/web/views/@default/settings/cluster/index.js
+++ src/main/web/views/@default/settings/cluster/index.js
@@ -3,4 +3,19 @@ Tea.context(function () {
 		this.error = "重试中...";
 		this.$post(".connect").refresh();
 	};
+
+	/**
+	 * 同步
+	 */
+	this.isSyncing = false;
+
+	this.sync = function () {
+		this.isSyncing = true;
+		this.$post(".sync")
+			.success(function () {
+				this.$delay(function () {
+					window.location.reload();
+				}, 1000);
+			});
+	};
 });
