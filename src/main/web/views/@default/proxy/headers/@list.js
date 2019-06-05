f28ea922853994c4b9240102c1c57b12315f463f
刘祥超 <iwind.liu@gmail.com>
2019-06-05 11:27:04 +0800

    [proxy]自定义Header增加设置请求Header

--- src/main/web/views/@default/proxy/headers/@list.js
+++ src/main/web/views/@default/proxy/headers/@list.js
@@ -1,51 +1,64 @@
 Tea.context(function () {
-    this.loaded = false;
+	this.loaded = false;
 
-    this.headers = [];
-    this.ignoreHeaders = [];
+	this.requestHeaders = [];
+	this.headers = [];
+	this.ignoreHeaders = [];
 
-    this.query = Tea.serialize(this.headerQuery);
-    this.from = encodeURIComponent(window.location.toString());
+	this.query = Tea.serialize(this.headerQuery);
+	this.from = encodeURIComponent(window.location.toString());
 
-    this.$delay(function () {
-        this.loadData();
-    });
+	this.$delay(function () {
+		this.loadData();
+	});
 
-    this.loadData = function () {
-        this.$get("/proxy/headers/data")
-            .params(this.headerQuery)
-            .success(function (resp) {
-                this.headers = resp.data.headers.$map(function (k, v) {
-                    return v;
-                });
-                this.ignoreHeaders = resp.data.ignoreHeaders;
-                this.loaded = true;
-            });
-    };
+	this.loadData = function () {
+		this.$get("/proxy/headers/data")
+			.params(this.headerQuery)
+			.success(function (resp) {
+				this.headers = resp.data.headers;
+				this.requestHeaders = resp.data.requestHeaders;
+				this.ignoreHeaders = resp.data.ignoreHeaders;
+				this.loaded = true;
+			});
+	};
 
-    this.deleteIgnoreHeader = function (header) {
-        if (!window.confirm("确定要删除此Header吗？")) {
-            return;
-        }
-        var query = this.headerQuery;
-        query["name"] = header;
-        this.$post("/proxy/headers/deleteIgnore")
-            .params(query)
-            .success(function () {
-                window.location.reload();
-            });
-    };
+	this.deleteIgnoreHeader = function (header) {
+		if (!window.confirm("确定要删除此Header吗？")) {
+			return;
+		}
+		var query = this.headerQuery;
+		query["name"] = header;
+		this.$post("/proxy/headers/deleteIgnore")
+			.params(query)
+			.success(function () {
+				window.location.reload();
+			});
+	};
 
-    this.deleteHeader = function (header) {
-        if (!window.confirm("确定要删除此Header吗？")) {
-            return;
-        }
-        var query = this.headerQuery;
-        query["headerId"] = header.id;
-        this.$post("/proxy/headers/delete")
-            .params(query)
-            .success(function () {
-                window.location.reload();
-            });
-    };
+	this.deleteHeader = function (header) {
+		if (!window.confirm("确定要删除此Header吗？")) {
+			return;
+		}
+		var query = this.headerQuery;
+		query["headerId"] = header.id;
+		this.$post("/proxy/headers/delete")
+			.params(query)
+			.success(function () {
+				window.location.reload();
+			});
+	};
+
+	this.deleteRequestHeader = function (header) {
+		if (!window.confirm("确定要删除此请求Header吗？")) {
+			return;
+		}
+		var query = this.headerQuery;
+		query["headerId"] = header.id;
+		this.$post("/proxy/headers/deleteRequestHeader")
+			.params(query)
+			.success(function () {
+				window.location.reload();
+			});
+	};
 });
---
+++ src/main/web/views/@default/proxy/headers/addRequestHeader.js
@@ -0,0 +1,19 @@
+Tea.context(function () {
+	this.$delay(function () {
+		this.$find("form input[name='name']").focus();
+	});
+
+	this.addSuccess = function () {
+		alert("保存成功");
+		window.location = this.from;
+	};
+
+	/**
+	 * 高级选项
+	 */
+	this.advancedOptionsVisible = false;
+
+	this.showAdvancedOptions = function () {
+		this.advancedOptionsVisible = !this.advancedOptionsVisible;
+	};
+});
