985a0485143b0699e5100afb4eac01c2f82787e1
刘祥超 <iwind.liu@gmail.com>
2019-08-12 15:24:18 +0800

    搜索支持上下键移动

--- src/main/web/views/@default/search/index.js
+++ src/main/web/views/@default/search/index.js
@@ -1,15 +1,41 @@
 Tea.context(function () {
 	var that = this;
 
+	this.selectedIndex = -1;
+
 	this.$delay(function () {
 		this.$find("form input[name='keyword']").focus();
+
+		var that = this;
 
 		document.addEventListener("keyup", function (e) {
 			if (e == null || e.key == null || e.target == null) {
 				return;
 			}
-			if (e.key.toString() == "Escape") {
+			var keyString = e.key.toString();
+			if (keyString == "Escape") {
 				window.history.back();
+				return;
+			}
+			if (keyString == "ArrowDown") {
+				that.selectedIndex++;
+				if (that.selectedIndex >= that.results.length) {
+					that.selectedIndex = 0;
+				}
+				return;
+			}
+			if (keyString == "ArrowUp") {
+				that.selectedIndex--;
+				if (that.selectedIndex < 0) {
+					that.selectedIndex = 0;
+				}
+				return;
+			}
+			if (keyString == "Enter") {
+				if (that.selectedIndex > -1) {
+					window.location = that.results[that.selectedIndex].link;
+				}
+				return;
 			}
 			if (e.target.tagName == "INPUT") {
 				return;
@@ -32,6 +58,7 @@ Tea.context(function () {
 
 	this.changeKeyword = function () {
 		teaweb.set("globalSearchKeyword", this.keyword);
+		this.selectedIndex = -1;
 		if (this.keyword.length == 0) {
 			this.results = [];
 			return;
