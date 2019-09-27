c1b0d77176ddafd91e37118c75369d5803dff15c
刘祥超 <iwind.liu@gmail.com>
2019-09-27 10:46:26 +0800

    使用build-all.sh脚本可以单独构建某个平台版本

--- src/main/build-all.sh
+++ src/main/build-all.sh
@@ -4,13 +4,24 @@
 
 . utils.sh
 
-. build-all-agents.sh
-. build-all-installers.sh
+if [ "$1" = "" ]
+then
+	. build-all-agents.sh
+	. build-all-installers.sh
 
-. build-linux-32.sh
-. build-linux-64.sh
-. build-linux-arm-64.sh
-. build-linux-arm.sh
-. build-darwin.sh
-. build-windows-32.sh
-. build-windows-64.sh
+	. build-linux-32.sh
+	. build-linux-64.sh
+	. build-linux-arm-64.sh
+	. build-linux-arm.sh
+	. build-darwin.sh
+	. build-windows-32.sh
+	. build-windows-64.sh
+elif [ "$2" != "" ]
+then
+		export GOOS=$1
+		export GOARCH=$2
+		build
+elif [ "$2" = "" ]
+then
+	echo "Usage: ${0} OS ARCH"
+fi
