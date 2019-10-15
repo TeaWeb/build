d4c80957e548f9d8b3aa4589d8e5cafeca813443
刘祥超 <iwind.liu@gmail.com>
2019-10-16 00:16:10 +0800

    为MySQL和PostgreSQL增加手动清理数据表和自动清理数据表功能

---
+++ src/main/web/views/@default/settings/postgres/cleanUpdate.js
@@ -0,0 +1,17 @@
+Tea.context(function () {
+	if (this.accessLog == null) {
+		this.accessLog = {
+			"cleanHour": "",
+			"keepDays": ""
+		};
+	}
+
+	this.$delay(function () {
+		this.$find("form input[name='accessLogCleanHour']").focus();
+	});
+
+	this.saveSuccess = function () {
+		alert("保存成功");
+		window.location = "/settings/postgres/clean";
+	};
+});
