251a7935dbf9e5039b640a82afae23c8c4c7f06a
刘祥超 <iwind.liu@gmail.com>
2019-10-26 16:19:57 +0800

    界面优化

--- src/main/web/views/@default/login/index.js
+++ src/main/web/views/@default/login/index.js
@@ -29,6 +29,26 @@ Tea.context(function () {
 		this.isSubmitting = true;
 	};
 
+	this.failSubmit = function (resp) {
+		if (resp.message != null && resp.message.length > 0) {
+			alert(resp.message);
+			return;
+		}
+		if (resp.errors != null && resp.errors.length > 0) {
+			var err = resp.errors[0];
+			if (err.messages != null && err.messages.length > 0) {
+				alert(err.messages[0]);
+			}
+			if (err.param == "username") {
+				this.$refs.usernameRef.focus();
+			} else if (err.param == "password") {
+				this.$refs.passwordRef.focus();
+			} else if (err.param == "refresh") {
+				window.location.reload();
+			}
+		}
+	};
+
 	this.doneSubmit = function () {
 		this.isSubmitting = false;
 	};
